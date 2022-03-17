2777a434cf0ccce8c66f4faf86f4385c17055d56
Linto Thomas <linto@BourntecsiMac2.domain.name>
2022-03-17 17:40:26 +0530

    linto

--- src/components/login/Loginform.jsx
+++ src/components/login/Loginform.jsx
@@ -26,7 +26,6 @@ export default function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     axios
-<<<<<<< HEAD
       .post("http://localhost:8000/api/user-login", {
         email: email,
         password: password,
@@ -71,40 +70,6 @@ export default function Login() {
             setErrors({ errMsg: "" });
           }, 2000);
         }
-=======
-    .post("http://localhost:8000/api/user-login", {
-      email: email,
-      password:password,
-    }) .then((response) => {
-      if (response.data.status === 200) {
-      //  setProfile(response);
-     
-      let user = { ...response.data.user };
-      user.token = response.data.token;
-      let username=user.name;
-      user = JSON.stringify(user);
-      console.log(user);
-      setUser(user);
-      localStorage.setItem("user", user);
-    
-      if(username==='admin')
-      {
-        history.push("/Dashboard");
-     
-      }
-      else {
-        history.push("/Employeeprofile");
-      }
-      }
-      if (
-        response.data.status === "failed" &&
-        response.data.success === undefined
-      )
-      {
-      setErrors({
-        errMsgEmail: response.data.validation_error.email,
-        errMsgPwd: response.data.validation_error.password,
->>>>>>> b8f6d2abcaf6069d49088153e5c6037d90825854
       });
   };
 
