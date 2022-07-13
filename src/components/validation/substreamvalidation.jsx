4e9b91b2b2c8119af02cbfa6162626d9f18677b7
kishan <kishan.c@bourntec.com>
2022-07-13 11:50:20 +0530

    kishan

--- src/components/validation/substreamvalidation.jsx
+++ src/components/validation/substreamvalidation.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function substreamvalidation(values) {
   let errors = {};
+  if (!values.stream_name.trim()) {
+    errors.stream_name = "Stream name required";
+  }
+  if (!values.substream_name.trim()) {
+    errors.substream_name = "Substream name required";
+  }
  
   return errors;
 }
