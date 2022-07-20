af0fbc906db0622fa17ca44ffa925bdfbb919b01
kishan <kishan.c@bourntec.com>
2022-07-20 16:36:04 +0530

    kishan

--- src/components/pages/Noticeform.jsx
+++ src/components/pages/Noticeform.jsx
@@ -39,7 +39,6 @@ const Noticeform = (noticeperiod_validation) => {
     const response = axios.post("http://localhost:8000/api/add_notice", values);
     response.then(function(res) {
       if (res.data.status === 200) {
-        //console.log(res.data.message);
         swal({
           title: "Good job!",
           text: "Job added successfully",
